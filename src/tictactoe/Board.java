3d621a8adfe29d8cd792d408c21bbff611628833
nico <nico@192.168.2.120>
2016-10-20 13:03:40 +0200

    structuring

--- src/tictactoe/Board.java
+++ src/tictactoe/Board.java
@@ -5,29 +5,37 @@ import java.util.Map;
 
 /**
  * Board.
- * 
+ *
  * Board class that represents the 3 by 3 field we play on.
- * 
+ *
  * @author nico
  */
 public class Board {
-   
+
     //Object Variables
     //==========================================================================
-    
-    private final Map<Integer,Tokens> board;
-    
+    private final Map<Integer, Tokens> board;
+
     //Constructors
     //==========================================================================
-
     /**
      * Board.
-     * 
+     *
      * Initializes a new board to play on.
      */
     public Board() {
         this.board = new HashMap<>();
     }
+
+    //Methods (Public)
+    //==========================================================================
     
+    public void placeToken(Tokens toPlace) {
+        
+    }
+    
+    
+    //Methods (Private)
+    //==========================================================================
     
 }
